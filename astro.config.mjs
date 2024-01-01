fa1675e985493f9db8809e9cdf64886bc70752f3
Quynh Nguyen <q.nguyen@shopware.com>
2024-01-01 13:55:45 +0700

    Fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
-import cloudflare from '@astrojs/cloudflare';
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
